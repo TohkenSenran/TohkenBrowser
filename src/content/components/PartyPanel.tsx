e92ee086b95ffd455a677573bdd8cfb764a6f34b
TohkenSenran <tohkenbrowser@gmail.com>
2020-01-08 10:02:45 +0900

    divide partyPanel components

--- src/content/components/PartyPanel.tsx
+++ src/content/components/PartyPanel.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 
-import { Box, Card, CardContent, Fade } from '@material-ui/core';
-import { Transition } from 'react-transition-group';
+import { Card, CardContent } from '@material-ui/core';
 
-import { partyMemberNo, partyNo } from '../../constants';
 import { PartyPanelProps } from '../containers/PartyPanel';
-import { partyConverter } from '../models/partyConverter';
 import { Equips } from '../states/responseJson/Equip';
 import { Parties } from '../states/responseJson/Party';
 import { Swords } from '../states/responseJson/Sword';
-import SelectSpeedCorrectMenu from './SelectSpeedCorrectMenu';
-import SwordPanel from './SwordPanel';
+import PartyPanelContents from './PartyPanelContents';
 
 const PartyPanel: React.FC<PartyPanelProps> = (props) => {
   const handleClick = () => {
@@ -23,128 +19,28 @@ const PartyPanel: React.FC<PartyPanelProps> = (props) => {
   const equipData: Equips = props.equip;
   // console.log('Update Party');
   // console.log(`updatePartyPanel ${props.partyPanel.correct}`);
-  const parties: JSX.Element[] = [];
 
   // 結成の時に表示切替
-  if (props.partyPanel.enableExtendView && (props.page) &&
+  // props.partyPanel.enableExtendView &&
+  if ((props.page) &&
     (((props.page.indexOf('party/') > -1) && (props.page !== 'party/get_sally_party_info')) ||
       (props.page.indexOf('equip/') > -1))) {
-    // console.log('in party');
+    // console.log('in party', props.partyPanel.extendView);
     if (!props.partyPanel.extendView) {
       props.clickPartyProps(false);
     }
   } else {
-    // console.log('not party');
+    // console.log('not party', props.partyPanel.extendView);
     if (props.partyPanel.extendView) {
       props.clickPartyProps(true);
     }
   }
 
-  for (let i: number = 0; i < partyNo; i += 1) {
-    const party: JSX.Element[] = [];
-    for (let j: number = 0; j < partyMemberNo; j += 1) {
-      // console.log(`partyName ${partyData[i + 1].party_name}`);
-      party.push(<SwordPanel
-        swords={swordData}
-        serialId={partyData[i + 1].slot[j + 1].serial_id}
-        selectCorrect={props.partyPanel.correct}
-        selectTextType={props.partyPanel.textType}
-        horseDisable={props.partyPanel.horseDisable}
-        equips={equipData}
-      />);
-    }
-
-    const partyState = partyConverter(partyData[i + 1], swordData, equipData, props.date);
-    const firstStateStyle: React.CSSProperties = {
-      display: 'block',
-      whiteSpace: 'nowrap',
-    };
-    const secondStateStyle: React.CSSProperties = {
-      display: 'block',
-      letterSpacing: -1,
-      whiteSpace: 'nowrap',
-    };
-
-    // 表示系を生成
-    let partyFirstStyle: React.CSSProperties = { ...firstStateStyle };
-    switch (partyState.state) {
-      case '遠征中':
-        partyFirstStyle = { ...firstStateStyle, color: '#9644E3' };
-        break;
-      case '出陣中':
-        partyFirstStyle = { ...firstStateStyle, color: '#A24E36' };
-        break;
-    }
-
-    const partyStateComponent: JSX.Element = (
-      <Box>
-        <Box style={partyFirstStyle}>
-          {partyState.state}
-        </Box>
-        <Box style={secondStateStyle}>
-          {partyState.remainingTime}
-        </Box>
-      </Box>
-    );
-
-    const memberFirstStyle: React.CSSProperties = { ...firstStateStyle, letterSpacing: -1 };
-    let memberSecondStyle: React.CSSProperties = { ...secondStateStyle };
-    const totalScout: number = parseInt(partyState.memberScout.replace(/[^0-9^\.]/g, ''));
-    if (totalScout < 320) {
-      memberSecondStyle = { ...secondStateStyle, color: 'navy' };
-    } else if (totalScout < 500) {
-      memberSecondStyle = { ...secondStateStyle, color: 'darkorange' };
-    } else {
-      memberSecondStyle = { ...secondStateStyle, color: 'crimson' };
-    }
-
-    const memberStateComponent: JSX.Element = (
-      <Box>
-        <Box style={memberFirstStyle}>
-          {partyState.memberLv}
-        </Box>
-        <Box style={memberSecondStyle}>
-          {partyState.memberScout}
-        </Box>
-      </Box>
-    );
-
-    // props内の配列のインデックスはjsonの影響で1からスタート
-    const tempParty = (
-      <Box onClick={handleClick} marginTop="4px" display="flex" flexDirection="row">
-        <Box height="66px" width="70px" padding="3px 0px">
-          <Box marginBottom="1px" fontStyle="bold" display="block">
-            {partyState.name}
-          </Box>
-          <Box marginLeft="3px">
-            {partyStateComponent}
-          </Box>
-        </Box>
-        <Box display="flex" flexDirection="row">
-          {party}
-        </Box>
-        <Box position="relative">
-          <Fade in={!props.partyPanel.extendView}>
-            <Box display="flex">
-              <React.Fragment />
-            </Box>
-          </Fade>
-          <Fade in={props.partyPanel.extendView}>
-            <Box display="flex" flexWrap="wrap" position="absolute" top="0px" left="0px" width="100px">
-              {memberStateComponent}
-            </Box>
-          </Fade>
-        </Box>
-      </Box>
-    );
-    parties.push(tempParty);
-  }
-
-  // 
   const partyPanelStyle = {
     extend: {
       height: 288,
       width: 760,
+      transition: 'all .5s ease',
     },
     normal: {
       height: 288,
@@ -153,15 +49,19 @@ const PartyPanel: React.FC<PartyPanelProps> = (props) => {
     },
   };
 
-  const partyPanel_NormalStyle: React.CSSProperties = { height: 288, width: 478 };
-  const partyPanel_ExtendStyle: React.CSSProperties = { height: 288, width: 578 };
-
   return (
     <Card
       style={props.partyPanel.extendView ? partyPanelStyle.extend : partyPanelStyle.normal}
     >
       <CardContent style={{ padding: 0, margin: 6 }}>
-        {parties}
+        <PartyPanelContents
+          partyData={partyData}
+          swordData={swordData}
+          equipData={equipData}
+          partyPanel={props.partyPanel}
+          date={props.date}
+          onClick={handleClick}
+        />
       </CardContent>
     </Card>
   );
--- src/content/components/PartyPanelContents.tsx
+++ src/content/components/PartyPanelContents.tsx
@@ -1,38 +1,26 @@
 import * as React from 'react';
 
-import { Box, Card, CardContent, Fade } from '@material-ui/core';
-import { Transition } from 'react-transition-group';
+import { Box, Fade } from '@material-ui/core';
 
 import { partyMemberNo, partyNo } from '../../constants';
-import { PartyPanelProps } from '../containers/PartyPanel';
 import { partyConverter } from '../models/partyConverter';
+import { PartyPanelState } from '../states/PartyPanelState';
 import { Equips } from '../states/responseJson/Equip';
 import { Parties } from '../states/responseJson/Party';
 import { Swords } from '../states/responseJson/Sword';
-import SelectSpeedCorrectMenu from './SelectSpeedCorrectMenu';
 import SwordPanel from './SwordPanel';
 
-const PartyPanelContents: React.FC<{ partyData: Parties, swordData: Swords, equipData: Equips }> =
-  ({ partyData, swordData, equipData }) => {
+const PartyPanelContents: React.FC<{
+  partyData: Parties,
+  swordData: Swords,
+  equipData: Equips,
+  partyPanel: PartyPanelState,
+  date: number,
+  onClick: () => void,
+}> =
+  ({ partyData, swordData, equipData, partyPanel, date, onClick }) => {
 
-    // console.log('Update Party');
-    // console.log(`updatePartyPanel ${props.partyPanel.correct}`);
     const parties: JSX.Element[] = [];
-
-    // 結成の時に表示切替
-    if (props.partyPanel.enableExtendView && (props.page) &&
-      (((props.page.indexOf('party/') > -1) && (props.page !== 'party/get_sally_party_info')) ||
-        (props.page.indexOf('equip/') > -1))) {
-      // console.log('in party');
-      if (!props.partyPanel.extendView) {
-        props.clickPartyProps(false);
-      }
-    } else {
-      // console.log('not party');
-      if (props.partyPanel.extendView) {
-        props.clickPartyProps(true);
-      }
-    }
 
     for (let i: number = 0; i < partyNo; i += 1) {
       const party: JSX.Element[] = [];
@@ -41,71 +29,48 @@ const PartyPanelContents: React.FC<{ partyData: Parties, swordData: Swords, equi
         party.push(<SwordPanel
           swords={swordData}
           serialId={partyData[i + 1].slot[j + 1].serial_id}
-          selectCorrect={props.partyPanel.correct}
-          selectTextType={props.partyPanel.textType}
-          horseDisable={props.partyPanel.horseDisable}
+          selectCorrect={partyPanel.correct}
+          selectTextType={partyPanel.textType}
+          horseDisable={partyPanel.horseDisable}
           equips={equipData}
         />);
       }
 
-      const partyState = partyConverter(partyData[i + 1], swordData, equipData, props.date);
-      const firstStateStyle: React.CSSProperties = {
+      const partyState = partyConverter(partyData[i + 1], swordData, equipData, date);
+
+      let partyStateStyle: React.CSSProperties = {
         display: 'block',
         whiteSpace: 'nowrap',
       };
-      const secondStateStyle: React.CSSProperties = {
+      const remainingTimeStyle: React.CSSProperties = {
         display: 'block',
         letterSpacing: -1,
         whiteSpace: 'nowrap',
       };
 
-      // 表示系を生成
-      let partyFirstStyle: React.CSSProperties = { ...firstStateStyle };
+      // 状態に合わせて表示色を変更
       switch (partyState.state) {
         case '遠征中':
-          partyFirstStyle = { ...firstStateStyle, color: '#9644E3' };
+          partyStateStyle = { ...partyStateStyle, color: '#9644E3' };
           break;
         case '出陣中':
-          partyFirstStyle = { ...firstStateStyle, color: '#A24E36' };
+          partyStateStyle = { ...partyStateStyle, color: '#A24E36' };
           break;
       }
 
       const partyStateComponent: JSX.Element = (
         <Box>
-          <Box style={partyFirstStyle}>
+          <Box style={partyStateStyle}>
             {partyState.state}
           </Box>
-          <Box style={secondStateStyle}>
+          <Box style={remainingTimeStyle}>
             {partyState.remainingTime}
           </Box>
         </Box>
       );
 
-      const memberFirstStyle: React.CSSProperties = { ...firstStateStyle, letterSpacing: -1 };
-      let memberSecondStyle: React.CSSProperties = { ...secondStateStyle };
-      const totalScout: number = parseInt(partyState.memberScout.replace(/[^0-9^\.]/g, ''));
-      if (totalScout < 320) {
-        memberSecondStyle = { ...secondStateStyle, color: 'navy' };
-      } else if (totalScout < 500) {
-        memberSecondStyle = { ...secondStateStyle, color: 'darkorange' };
-      } else {
-        memberSecondStyle = { ...secondStateStyle, color: 'crimson' };
-      }
-
-      const memberStateComponent: JSX.Element = (
-        <Box>
-          <Box style={memberFirstStyle}>
-            {partyState.memberLv}
-          </Box>
-          <Box style={memberSecondStyle}>
-            {partyState.memberScout}
-          </Box>
-        </Box>
-      );
-
-      // props内の配列のインデックスはjsonの影響で1からスタート
       const tempParty = (
-        <Box onClick={handleClick} marginTop="4px" display="flex" flexDirection="row">
+        <Box onClick={onClick} marginTop="4px" display="flex" flexDirection="row">
           <Box height="66px" width="70px" padding="3px 0px">
             <Box marginBottom="1px" fontStyle="bold" display="block">
               {partyState.name}
@@ -118,46 +83,27 @@ const PartyPanelContents: React.FC<{ partyData: Parties, swordData: Swords, equi
             {party}
           </Box>
           <Box position="relative">
-            <Fade in={!props.partyPanel.extendView}>
+            <Fade in={!partyPanel.extendView}>
               <Box display="flex">
                 <React.Fragment />
               </Box>
             </Fade>
-            <Fade in={props.partyPanel.extendView}>
+            <Fade in={partyPanel.extendView}>
               <Box display="flex" flexWrap="wrap" position="absolute" top="0px" left="0px" width="100px">
-                {memberStateComponent}
+                {'拡張表示'}
               </Box>
             </Fade>
           </Box>
         </Box>
       );
+
       parties.push(tempParty);
     }
 
-    // 
-    const partyPanelStyle = {
-      extend: {
-        height: 288,
-        width: 760,
-      },
-      normal: {
-        height: 288,
-        width: 478,
-        transition: 'all .5s ease',
-      },
-    };
-
-    const partyPanel_NormalStyle: React.CSSProperties = { height: 288, width: 478 };
-    const partyPanel_ExtendStyle: React.CSSProperties = { height: 288, width: 578 };
-
     return (
-      <Card
-        style={props.partyPanel.extendView ? partyPanelStyle.extend : partyPanelStyle.normal}
-      >
-        <CardContent style={{ padding: 0, margin: 6 }}>
-          {parties}
-        </CardContent>
-      </Card>
+      <React.Fragment>
+        {parties}
+      </React.Fragment>
     );
   };
 
