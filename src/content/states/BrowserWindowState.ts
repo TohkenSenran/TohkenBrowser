c204ff45d953398f9103477ed99e23289ccb3d9a
TohkenSenran <tohkenbrowser@gmail.com>
2020-01-14 00:38:18 +0900

    refactoring create window

--- src/content/states/BrowserWindowState.ts
+++
@@ -1,9 +0,0 @@
-export interface BrowserWindow {
-  position: { x: number, y: number };
-  size: { height: number, width: number };
-}
-
-export const initialBrowserWindow: BrowserWindow = {
-  position: { x: 100, y: 100 },
-  size: { height: 600, width: 800 },
-};
